e99f4349d2e86e6de89971b6272ad8e9de970780
Rod1on777 <rodionk533@gmail.com>
2024-03-16 19:27:13 +1100

    Correct installation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as ReactDOMClient from 'react-dom/client';
 
 
 
-ReactDOM.render(    //Вывод текста в блок с помощью команды
+/* ReactDOMClient.createRoot(    //Вывод текста в блок с помощью команды
     <div>
         <h1>Hi</h1>
         <h2> Hello </h2>
     </div>,
     document.getElementById('app'))
 
-ReactDOM.render(React.createElement('input', {  //Вывод сообщения при наведении и нажатии на поле в консоль
+ReactDOMClient.createRoot(React.createElement('input', {  //Вывод сообщения при наведении и нажатии на поле в консоль
     placeholder: 'Help text',
     onClick: () => console.log("Clicked"),
     onMouseEnter: () => console.log("Mouse over")
 }), document.getElementById('app1'))
 
-ReactDOM.render(<input placeholder='Help text' />, document.getElementById('app2')) //Более удобный формат вывода
+ReactDOMClient.createRoot(<input placeholder='Help text' />, document.getElementById('app2')) //Более удобный формат вывода
 
-
+*/
 
 
 
@@ -37,12 +34,12 @@ const elements = (
     <div className="elements">
         <h1> Elements </h1>
         <input placeholder={helpText} onClick={inputClick} onMouseEnter={mouseOver} /> //Создание констатны элемента input, вополняющую функцию inputClick при клике и mjuseOver при наведении
-        <p> {helpText == "Help text bebriki" ? "Yes" : "No"}</p>    //Условие: если helpText = "Help text bebriki", вывести yes, если нет, то no
+        <p> {helpText === "Help text bebriki" ? "Yes" : "No"}</p>    //Условие: если helpText = "Help text bebriki", вывести yes, если нет, то no
     </div>
 )
-const app = document.getElementById("app3")
+const app = ReactDOMClient.createRoot(document.getElementById("app3"))
 
-ReactDOM.render(elements, app)
+app.render(elements)
 
 
 
